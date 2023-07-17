672b2e5841c6ba2a69ce5b17f2eda2f792cc0cef
aza547 <alexander-12345@hotmail.co.uk>
2023-07-17 20:20:13 +0100

    initial stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/aza547/wow-recorder/releases/latest"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Warcraft Recorder!
         </a>
       </header>
     </div>
